c0e4e34f5b8710f706b0ca93b7e7e8d4e0cd3e7f
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2021-03-29 16:37:30 -0300

    wip create ranking nome

--- Api_nomes_populares/lib/ibge_dados.rb
+++ Api_nomes_populares/lib/ibge_dados.rb
@@ -18,14 +18,14 @@ class IbgeDados
     response = Faraday.get('https://servicodados.ibge.gov.br/api/v1/localidades/distritos') 
     json = JSON.parse(response.body, symbolize_names: true)
     json.map do |dados|
-    puts dados[:id],dados[:nome],dados[:municipio][:microrregiao][:mesorregiao][:UF][:sigla]
-    
+    #puts dados[:municipio][:microrregiao][:mesorregiao][:UF][:nome]
+    puts dados[:id],dados[:nome],dados[:municipio][:microrregiao][:mesorregiao][:UF] dados[:id], dados[:nome], dados[:sigla]
   end 
-     
   end
+
     self.localidades_all
 #{:id=>421985305, :nome=>"Zortéa", :municipio=>{:id=>4219853, :nome=>"Zortéa", :microrregiao=>{:id=>42009, :nome=>"Curitibanos", :mesorregiao=>{:id=>4203, :nome=>"Serrana", :UF=>{:id=>42, :sigla=>"SC", :nome=>"Santa Catarina", :regiao=>{:id=>4, :sigla=>"S", :nome=>"Sul"}}}}, :"regiao-imediata"=>{:id=>420008, :nome=>"Joaçaba - Herval d'Oeste", :"regiao-intermediaria"=>{:id=>4204, :nome=>"Chapecó", :UF=>{:id=>42, :sigla=>"SC", :nome=>"Santa Catarina", :regiao=>{:id=>4, :sigla=>"S", :nome=>"Sul"}}}}}}
-
+  
 
 end
 #:UF=>{:id=>42, :sigla=>"SC", :nome=>"Santa Catarina", 
